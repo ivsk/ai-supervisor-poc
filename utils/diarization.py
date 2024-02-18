8df53c3e1e942afed34b82a59ca0ee706c6b7ab4
ivsk <ivakevics.krisztian@gmail.com>
2024-02-18 20:52:24 +0100

    Modifying diarization

--- utils/diarization.py
+++ utils/diarization.py
@@ -1,6 +1,10 @@
 from pyannote.audio import Pipeline
 import torchaudio
 import configparser
+import torch
+import whisper
+from whisperx import load_align_model, align
+from whisperx.diarize import assign_word_speakers
 
 config = configparser.ConfigParser()
 
@@ -10,12 +14,32 @@ pipeline = Pipeline.from_pretrained(
   "pyannote/speaker-diarization-3.1",
   use_auth_token=api_key)
 
-diarization = pipeline("first_test.mp4")
+pipeline.to(torch.device("cuda"))
+
+diarization = pipeline("first_test_trimmed.wav", num_speakers=2)
 
 with open("first_test.rttm", "w") as rttm:
   diarization.write_rttm(rttm)
 
-from pyannote.audio.pipelines.utils.hook import ProgressHook
-with ProgressHook() as hook:
-    diarization = pipeline("first_test.mp4", hook=hook)
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+model_name = "large"
+model = whisper.load_model(model_name, DEVICE)
+script = model.transcribe("first_test_trimmed.wav")
+
+model_a, metadata = load_align_model(language_code=script["language"], device=DEVICE)
+script_aligned = align(script["segments"], model_a, metadata, "first_test_trimmed.wav", DEVICE)
+
+# Align Speakers
+result_segments, word_seg = list(assign_word_speakers(
+    diarization, script_aligned).values())
+transcribed = []
+for result_segment in result_segments:
+    transcribed.append(
+        {
+            "start": result_segment["start"],
+            "end": result_segment["end"],
+            "text": result_segment["text"],
+            "speaker": result_segment["speaker"],
+        }
+    )
--- utils/scrape_yt.py
+++ utils/scrape_yt.py
@@ -1,20 +1,28 @@
 from pytube import YouTube
-import re
 import os
-import json
+import subprocess
 
 yt = YouTube("https://youtu.be/oWMNskk8nzY?si=plumQqVv8kA0PSgv")
 
 audio_stream = yt.streams.get_audio_only()
 audio_stream.download(output_path='', filename='first_test.mp4')
 
+def remove_first_n_seconds(input_file, output_file, start_time):
+    command = ['ffmpeg', '-i', input_file, '-ss', str(start_time), '-acodec', 'copy', output_file]
+    try:
+        subprocess.run(command, check=True)
+        print(f"Successfully removed the first {start_time} seconds: {output_file}")
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred: {e}")
 
-s = dict()
 
-for v in vids:
-    result = model.transcribe(f"nail_tips/{v}")
-    s[v] = result["text"]
+def convert_mp4_to_wav(input_file, output_file):
+    command = ['ffmpeg', '-i', input_file, '-vn', '-acodec', 'pcm_s16le', '-ar', '44100', '-ac', '2', output_file]
+    try:
+        subprocess.run(command, check=True)
+        print(f"Conversion successful: {output_file}")
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred during conversion: {e}")
 
-with open("nail_tips.json", "w", encoding="UTF-8") as outfile:
-    json.dump(s, outfile)
-
+convert_mp4_to_wav("first_test.mp4", "first_test.wav")
+remove_first_n_seconds("first_test.wav", "first_test_trimmed.wav", 46)
