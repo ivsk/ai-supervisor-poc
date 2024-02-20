234c52be494253ab1a4c7ddabb588aad59593b95
ivsk <ivakevics.krisztian@gmail.com>
2024-02-20 22:26:44 +0100

    Modifying pipeline

--- utils/diarization.py
+++ utils/diarization.py
@@ -5,37 +5,54 @@ import torch
 import whisper
 from transformers import pipeline
 from speechbox import ASRDiarizationPipeline
+from sortedcontainers import SortedDict
 
+def sorted_dict_to_json(sorted_dict):
+    sorted_dict = sorted_dict
+    result_list = []
+
+    for segment, info in sorted_dict.items():
+        start, end = segment.start, segment.end
+
+        # Iterate through the dictionary items for each segment
+        for track, label in info.items():
+            result_list.append({
+                'segment': {'start': start, 'end': end},
+                'track': track,  # Now uses the actual track from the dictionary
+                'label': label
+            })
+    return result_list
 
 
 config = configparser.ConfigParser()
 
 config.read("config.ini")
 api_key = config.get("HUGGINGFACE", "ApiKey")
-pipeline = Pipeline.from_pretrained(
+diarization_pipeline = Pipeline.from_pretrained(
   "pyannote/speaker-diarization-3.1",
   use_auth_token=api_key)
 
-pipeline.to(torch.device("cuda"))
+diarization_pipeline.to(torch.device("cuda"))
 
-diarization = pipeline("first_test_trimmed.wav", num_speakers=2)
+diarization = diarization_pipeline("first_test_trimmed.wav", num_speakers=2)
 
-with open("first_test.rttm", "w") as rttm:
-  diarization.write_rttm(rttm)
+a = sorted_dict_to_json(diarization._tracks)
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 asr_pipeline = pipeline(
     "automatic-speech-recognition",
     model="openai/whisper-large",
+    device=0
 )
 
 asr_pipeline(
-    sample["audio"].copy(),
+    "first_test_trimmed.wav",
     generate_kwargs={"max_new_tokens": 256},
-    return_timestamps=True,
+    return_timestamps=True
 )
 
 pipeline = ASRDiarizationPipeline(
     asr_pipeline=asr_pipeline, diarization_pipeline=diarization_pipeline
 )
+pipeline("first_test_trimmed.wav")
